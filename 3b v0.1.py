99d67eaa75bda0f3c452556ea1e4d098764fa4e7
jazupp <147801662+jazupp@users.noreply.github.com>
2023-10-30 12:57:34 +0100

    Update 3b v0.1.py

---
+++ 3b v0.1.py
@@ -0,0 +1,26 @@
+# intitializering
+# Comm klar? 
+# Robot klar?
+# Kamera klar?
+# Raspi klar?
+# Robot ude af frame?
+#
+
+
+#Tages billede
+#behandling pipeline
+#   skarpe kanter
+#   blurr
+#   evt. fjern robot fra frame¨
+
+
+#Recognition
+#   Emne A
+#   Emne B
+#Opsamling  
+
+
+#   Prio
+#   Pos/coords
+#   send data til robot
+#
